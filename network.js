4baad6e8a723e61f788b38b4b992813bfb3fbbba
Silver <rhyskoerber@gmail.com>
2024-01-01 18:12:58 +1030

    e

--- network.js
+++ network.js
@@ -1,5 +1,5 @@
 
-var ws = new WebSocket("wss://server.silverspace.online:443")
+var ws
 var connected = false
 var got = false
 
@@ -9,36 +9,52 @@ function sendMsg(sendData, bypass=false) {
 	}
 }
 
-ws.addEventListener("open", (event) => {
-	ws.send(JSON.stringify({connect: "analytics"}))
-})
+function connectToServer() {
+    connected = false
+    console.log("Connecting...")
+    if (ws) {
+        if (ws.readyState == WebSocket.OPEN) {
+			ws.close()
+		}
+    }
+    ws = new WebSocket("wss://server.silverspace.online:443")
+    ws.addEventListener("open", (event) => {
+        ws.send(JSON.stringify({connect: "analytics"}))
+    })
+    
+    ws.addEventListener("message", (event) => {
+        var msg = JSON.parse(event.data)
+        if ("connected" in msg) {
+            connected = true
+            console.log("Connected")
+            getViews(apps[selected])
+            getClicks(cpps[selected])
+        }
+        if ("ping" in msg) {
+            sendMsg({ping: true})
+        }
+        if ("views" in msg) {
+            if (!got) {
+                got = true
+                clicks = null
+            }
+            views = msg.views
+        }
+        if ("clicks" in msg) {
+            if (!got) {
+                got = true
+                views = null
+            }
+            clicks = msg.clicks
+        }
+        if ("online" in msg) {
+            online = msg.online
+        }
+    })
+    ws.addEventListener("close", (event) => {
+		console.log("Disconnected")
+		connectToServer()
+	})
+}
 
-ws.addEventListener("message", (event) => {
-    var msg = JSON.parse(event.data)
-    if ("connected" in msg) {
-        connected = true
-        console.log("Connected")
-        getViews(apps[selected])
-        getClicks(cpps[selected])
-    }
-    if ("ping" in msg) {
-        sendMsg({ping: true})
-    }
-    if ("views" in msg) {
-        if (!got) {
-            got = true
-            clicks = null
-        }
-        views = msg.views
-    }
-    if ("clicks" in msg) {
-        if (!got) {
-            got = true
-            views = null
-        }
-        clicks = msg.clicks
-    }
-    if ("online" in msg) {
-        online = msg.online
-    }
-})
+connectToServer()
