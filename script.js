f17d583fc6d9e5fda7ad7817f38cd64295c8f7cf
Silver <rhyskoerber@gmail.com>
2024-04-20 20:09:01 +1000

    e

--- script.js
+++ script.js
@@ -172,7 +172,7 @@ function update(timestamp) {
 
     ui.text(mouse.x+10, mouse.y+15+15*su, 25*su, hoverText)
 
-    ui.text(50*su, 30*su, 35*su, Math.round(latencyV)+"ms")
+    ui.text(80*su, 50*su, 50*su, Math.round(latencyV)+"ms")
 
     input.updateInput()
 }
